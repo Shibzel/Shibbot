231fee6e1e198ff0a56e2b7d6884a2da2108d973
JeanTheShiba <jeanlfbr+1@gmail.com>
2022-12-11 15:58:41 +0100

    Updating `misc` cog
    - Added `user-info` command

--- src/cogs/misc/__init__.py
+++ src/cogs/misc/__init__.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import bridge, commands
 
-from src import Shibbot, BaseCog
+from src import Shibbot, BaseCog, relative_timestamp, date_timestamp
 from .lang.en import English
 from .lang.fr import French
 
@@ -24,7 +24,7 @@ class MiscCog(BaseCog):
     
     @bridge.bridge_command(name="avatar", aliases=["av"], description="Gives the profile picture.", description_localization={"fr": "Donne l'image de profil."},
                            options=[discord.Option(discord.User, name="user", name_localizations={"fr": "utilisateur"},
-                                                   description="The user.", description_localizations={"fr": "L'utilisateur."})])
+                                                   description="The user whose profile picture you want.", description_localizations={"fr": "L'utilisateur dont vous souhaitez l'image de profil."}, required=False)])
     @commands.cooldown(3, 10, commands.BucketType.user)
     async def get_avatar(self, ctx: bridge.BridgeApplicationContext, user: discord.User = None):
         user = user if user else ctx.author
@@ -38,8 +38,35 @@ class MiscCog(BaseCog):
 
 
     @bridge.bridge_command(name="user-info", aliases=["uinfo", "userinfo"], description="Gives info about an account.",
-                           description_localizations={"fr": "Donne des infos sur un compte."}, options=[discord.Option(name="user", name_localizations={"fr": "utilisateur"},
-                           description="The user.", description_localizations={"fr": "L'utilisateur."})])
+                           description_localizations={"fr": "Donne des infos sur un compte."}, options=[discord.Option(discord.User, name="user",
+                           name_localizations={"fr": "utilisateur"}, description="The user you want more info about.",
+                           description_localizations={"fr": "L'utilisateur sur lequel voulez plus d'infos."}, required=False)])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def get_user_info(self, ctx: bridge.BridgeApplicationContext, user: discord.User = None):
         user = user if user else ctx.author
+        lang = await self.get_lang(ctx)
+
+        async with ctx.typing():
+            if ctx.guild:
+                potential_member = ctx.guild.get_member(user.id)
+                if potential_member:
+                    user: discord.Member = potential_member
+
+            embed = discord.Embed(title=lang.GET_USER_INFO_TITLE, color=user.accent_color or discord.Color.dark_gold())
+            if user.avatar:
+                embed.set_thumbnail(url=user.avatar)
+            embed.add_field(name=lang.GET_USER_INFO_FIELD1_NAME, value=lang.GET_USER_INFO_FIELD1_VALUE.format(user=user, user_id=user.id,
+                                                                                                              date_creation_date=date_timestamp(user.created_at),
+                                                                                                              relative_creation_date=relative_timestamp(user.created_at),
+                                                                                                              is_bot='✔' if user.bot else '❌', common_serv=len(user.mutual_guilds)))
+            if potential_member:
+                status = {"online": "🟢", "dnd": "⛔", "idle": "🌙", "offline": "🔌"}
+                embed.add_field(name=lang.GET_USER_INFO_FIELD2_NAME, value=lang.GET_USER_INFO_FIELD2_VALUE.format(nickname=user.display_name, 
+                                                                                                                  joined_at=date_timestamp(user.joined_at), 
+                                                                                                                  relative_joined_at=relative_timestamp(user.joined_at),
+                                                                                                                  activity=user.activity.name if user.activity else 'N/A',
+                                                                                                                  status=status[str(user.status)], top_role=user.top_role.mention))
+            if user.banner:
+                embed.set_image(url=user.banner.url)
+
+        await ctx.respond(embed=embed)
--- src/cogs/misc/lang/en.py
+++ src/cogs/misc/lang/en.py
@@ -1,2 +1,8 @@
 class English:
     DEFAULT_FOOTER = "Requested by {author}."
+
+    GET_USER_INFO_TITLE = "User Info"
+    GET_USER_INFO_FIELD1_NAME = "__Main Information__"
+    GET_USER_INFO_FIELD1_VALUE = "**#️⃣ Username and tag :** `{user}`\n**🆔 ID :** `{user_id}`\n**⏲ Account created on :** {date_creation_date} ({relative_creation_date})\n**🤖 Is a bot :** `{is_bot}`\n**:busts_in_silhouette: Servers in common :** `{common_serv}`"
+    GET_USER_INFO_FIELD2_NAME = "__Member Related Info__"
+    GET_USER_INFO_FIELD2_VALUE = "**🎭 Nickname :** `{nickname}`\n**🚪Joined the server on :** {joined_at} ({relative_joined_at})\n**🎨 Activity :** `{activity}`\n**Status :** `{status}`\n**Top role :** {top_role}"

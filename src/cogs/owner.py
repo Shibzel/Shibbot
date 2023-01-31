52872335802b524a5fd919b861c2054bc8512372
Shibzel <jeanlfbr+1@gmail.com>
2023-01-31 18:00:56 +0100

    Update owner.py

--- src/cogs/owner.py
+++ src/cogs/owner.py
@@ -25,7 +25,7 @@ class OwnerCommands(commands.Cog):
     async def _on_cog(self, ctx, method, cog_name, method_name):
         try:   
             method(cog_name)
-            await ctx.send(f"{method_name.title()}ed cog '{cog_name}' !")
+            await ctx.send(f"{method_name.title()}ed cog `{cog_name}` !")
         except Exception as error:
             message = f"Couldn't {method_name} '{cog_name}', the following error occured :"
             logger.error(message, error)
@@ -42,3 +42,19 @@ class OwnerCommands(commands.Cog):
     @commands.is_owner()
     async def unload(self, ctx: commands.Context, cog):
         await self._on_cog(ctx, self.bot.unload_extension, cog, "unload")
+        
+    @commands.command()
+    @commands.is_owner()
+    async def sync(self, ctx: commands.Context):
+        try:   
+            await self.bot.sync_commands()
+            await ctx.send("Synced all commands !")
+        except Exception as error:
+            message = "Couldn't sync commands."
+            logger.error(message, error)
+            await ctx.send(message, file=discord.File(fp=create_log_file(error)))
+        
+    @commands.command()
+    async def owner(self, ctx: commands.Context):
+        if ctx.author == self.bot.project_owner:
+            await ctx.send(f"Hey {self.bot.project_owner.mention}, I know you ! You wrote my code !")

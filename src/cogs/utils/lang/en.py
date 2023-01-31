dd436a8b333902eb238b3223a05d1c127d53a53e
Shibzel <jeanlfbr+1@gmail.com>
2023-01-31 18:01:20 +0100

    Updating cog `utils`

--- src/cogs/utils/lang/en.py
+++ src/cogs/utils/lang/en.py
@@ -1,2 +1,9 @@
 class English:
+    DEFAULT_FOOTER = "Requested by {user}"
+    
     SHORTEN_URL_WRONG_URL = "Please enter a valid url."
+    
+    TRANSLATE_TEXT_TITLE = "Translate"
+    TRANSLATE_TEXT_ORIGINAL = "Original"
+    TRANSLATE_TEXT_TRANSLATED = "Translated"
+    TRANSLATE_TEXT_LANG_CODE_ERR = "The language parameter must be a language code : https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes"
--- src/cogs/utils/utilities.py
+++ src/cogs/utils/utilities.py
@@ -2,10 +2,13 @@ import discord
 from discord.ext import bridge, commands
 from aiohttp import ClientSession
 from orjson import loads
+import aiogtrans
+import asyncio
 
 from src.core import Shibbot
 from src.models.cog import PluginCog
 from src.errors import ServiceUnavailableError, MissingArgumentsError
+from src.constants import LANGUAGES
 
 from . import English, French
 
@@ -36,7 +39,7 @@ class Utilities(PluginCog):
     @bridge.bridge_command(name="shorturl", aliases=["short"], description="Shorten a URL link.", description_localizations={"fr": "Raccourcit un lien URL."},
                            options=[discord.Option(name="url", description="The link to shorten.", description_localizations={"fr": "Le lien à raccourcir."})])
     @commands.cooldown(1, 7, commands.BucketType.default)
-    async def shorten_url(self, ctx: bridge.BridgeContext, url: str = None):
+    async def shorten_url(self, ctx: bridge.BridgeApplicationContext, url: str = None):
         if not url:
             raise MissingArgumentsError(ctx.command)
         
@@ -48,3 +51,35 @@ class Utilities(PluginCog):
             lang = await self.get_lang(ctx)
             raise MissingArgumentsError(ctx.command, error_case_msg=lang.SHORTEN_URL_WRONG_URL)
         await ctx.respond(content=result["shorturl"])
+    
+    @bridge.bridge_command(name="translate", aliases=["trans"], description="Tranlates text.", description_localizations={"fr": "Traduit du texte."},
+                           options=[discord.Option(name="language", name_localizations={"fr": "langage"},
+                                                   description="The language code (ex: 'fr', 'en').", description_localizations={"fr": "Le code de langage (ex: 'fr', 'en')."}),
+                                    discord.Option(name="sentence", name_localizations={"fr": "phrase"},
+                                                   description="The text to translate.", description_localizations={"fr": "Le texte à traduire."})])
+    @commands.cooldown(1, 7, commands.BucketType.default)
+    async def translate_text(self, ctx: bridge.BridgeApplicationContext, language: str = None, *, sentence: str = None):
+        if not language or not sentence:
+            raise MissingArgumentsError(ctx.command)
+        for i in LANGUAGES.items():
+            if language not in i:
+                lang = await self.get_lang(ctx)
+                raise MissingArgumentsError(ctx.command, lang.TRANSLATE_TEXT_LANG_CODE_ERR)
+            
+        async with ctx.typing():
+            try:
+                async with aiogtrans.GoogleTrans(target=language) as translator:
+                    result = await asyncio.wait_for(translator.translate(text=sentence), 5)
+            except aiogtrans.UnsupportedLanguage:
+                raise MissingArgumentsError(ctx.command, error_case_msg="")
+            except (aiogtrans.RequestError, aiogtrans.TooManyRequests, asyncio.TimeoutError):
+                raise ServiceUnavailableError
+            
+        lang = await self.get_lang(ctx)
+        embed = discord.Embed(color=0x4b8cf5)
+        embed.set_author(name=lang.TRANSLATE_TEXT_TITLE, icon_url="https://www.googlewatchblog.de/wp-content/uploads/google-translate-logo-1024x1024.png")
+        embed.add_field(name=lang.TRANSLATE_TEXT_ORIGINAL, value=sentence, inline=False)
+        embed.add_field(name=lang.TRANSLATE_TEXT_TRANSLATED, value=result, inline=False)
+        embed.set_footer(text=lang.DEFAULT_FOOTER.format(user=ctx.author), icon_url=ctx.author.avatar)
+
+        await ctx.respond(embed=embed)

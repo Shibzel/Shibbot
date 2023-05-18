3235dd88f685ef40d3384d46aa173518d3b4f67f
Shibzel <jeanlfbr+1@gmail.com>
2023-05-18 22:01:23 +0200

    Update Shibbot.py

--- Shibbot.py
+++ Shibbot.py
@@ -19,7 +19,6 @@ except ModuleNotFoundError as exc:
 
 
 CONFIG_FILE_PATH = "./config.toml"
-CONFIG_FP_EXEMPLE = CONFIG_FILE_PATH + ".exemple"
 SPLASH_TEXT_FP = "./misc/splash_text.toml"
 
 
@@ -38,10 +37,10 @@ class UncompletedOrMissing(ConfigError):
     """Raised when the a line in the cofig file is uncompleted or missing."""
     
 
-def ascii_art(logger: Logger):
+def ascii_art(splash_text_fp: str, logger: Logger):
     """Shows a beautiful ascii art with a splash text."""
     try:
-        with open(SPLASH_TEXT_FP, "rb") as f:
+        with open(splash_text_fp, "rb") as f:
             splash_text = toml.load(f)["SplashText"]
     except (FileNotFoundError, toml.TOMLDecodeError) as err:
         logger.error("An error occured while loading splash texts.", err)
@@ -63,21 +62,27 @@ def ascii_art(logger: Logger):
 """)
 
 
-def main():
+def main(
+    config_fp: str = CONFIG_FILE_PATH,
+    config_exemple_fp: str = CONFIG_FILE_PATH + ".exemple",
+    splash_text_dp: str = SPLASH_TEXT_FP,
+    checks: bool = True,
+    show_ascii: bool = True
+):
     """Main function. Do some checks and then starts the bot."""
     cls = Shibbot
 
     # Verifies if the config file exists
-    if not os.path.exists(CONFIG_FILE_PATH):
+    if not os.path.exists(config_fp):
         try:
-            copyfile(CONFIG_FP_EXEMPLE, CONFIG_FILE_PATH)
+            copyfile(config_exemple_fp, config_fp)
         except FileNotFoundError as exc:
-            raise MissingFile(CONFIG_FP_EXEMPLE) from exc
+            raise MissingFile(config_exemple_fp) from exc
         else:
             raise ConfigError(
-                f"Please fulfill the requirements inside of the {CONFIG_FILE_PATH} file.")
+                f"Please fulfill the requirements inside of the {config_fp} file.")
     # Loading config
-    with open(CONFIG_FILE_PATH, "rb") as toml_file:
+    with open(config_fp, "rb") as toml_file:
         config: dict[str, dict | object] = toml.load(toml_file)
     settings = config["Settings"]
     console = settings["UseConsole"]
@@ -117,16 +122,9 @@ def main():
     )
     kwargs["logger"] = logger
     
-    ascii_art(logger)
+    if show_ascii:
+        ascii_art(splash_text_dp, logger)
 
-    # Indicating Python version in debug logs
-    py_ver = python_version()
-    major, minor, _ = python_version_tuple()
-    logger.debug(f"Running on Python {py_ver}.")
-    if not 7 < int(minor) < 12 and int(major) != 3:
-        logger.warn(
-            f"Shibbot is not intended to run on version {py_ver} of Python.")
-        
     # Discord
     discord = config["Discord"]
     token = discord["Token"]
@@ -150,57 +148,66 @@ def main():
             ) from exc
     kwargs["instance_owners"] = instance_owners
 
-    # Code version
-    repo_name = "Shibzel/Shibbot"
-    try:
-        request = requests.get(
-            f"https://api.github.com/repos/{repo_name}/tags",
-            timeout=5)
-        response = orjson.loads(request.text)
-        assert request.status_code == 200
-        assert len(response) > 0
-        last_version = response[0]["name"]
-        if last_version == __version__:
-            logger.log("You're currently using the lastest version !")
-        else:
-            for release in response:
-                if release["name"] == __version__:
-                    logger.warn(
-                        f"You're not using the latest version '{__version__}' < '{last_version}'."
-                        f" Download the latest one here : https://github.com/{repo_name}/releases/")
-                    break
-            else:
-                logger.warn("You're currently using a wip/unlisted version.")
-    except (requests.RequestException, AssertionError) as err:
-        logger.error("Couldn't verify if the bot is up to date.", err)
+    if checks:
+        # Indicating Python version in debug logs
+        py_ver = python_version()
+        major, minor, _ = python_version_tuple()
+        logger.debug(f"Running on Python {py_ver}.")
+        if not 7 < int(minor) < 12 and int(major) != 3:
+            logger.warn(
+                f"Shibbot is not intended to run on version {py_ver} of Python.")
 
-    # Pterodactyl (for the hardware stats, optional)
-    pterodactyl = advanced["Pterodactyl"]
-    if pterodactyl["UsePterodactylAPI"]:
-        ptero_url = pterodactyl["URL"]
-        if ptero_url in ("", "https://"):
-            raise UncompletedOrMissing("Missing pterodactyl ptero_url.")
-        if ptero_url.endswith("/"):
-            raise Syntax("Your pterodactyl ptero_url mustn't end with '/'.")
-        ptero_token = pterodactyl["Token"]
-        if ptero_token in ("", "ptlc_"):
-            raise UncompletedOrMissing(
-                f"Missing pterodactyl token. You can it here : {ptero_url}account/api/")
-        ptero_server_id = pterodactyl["ServerID"]
-        if ptero_server_id == "":
-            raise UncompletedOrMissing(
-                "Missing pterodactyl server ID."
-                " The ID is at the end of the server's link in the panel :"
-                f" {ptero_url}server/" + ANSIEscape.underline + "1IdHere" + ANSIEscape.endc
-            )
-        cls = PterodactylShibbot
-        parameters = {
-            "ptero_url": ptero_url,
-            "ptero_token": ptero_token,
-            "ptero_server_id": ptero_server_id
-        }
-        kwargs.update(parameters)
-    logger.debug("All checks done and settings loaded.")
+        # Code version
+        repo_name = "Shibzel/Shibbot"
+        try:
+            request = requests.get(
+                f"https://api.github.com/repos/{repo_name}/tags",
+                timeout=5)
+            response = orjson.loads(request.text)
+            assert request.status_code == 200
+            assert len(response) > 0
+            last_version = response[0]["name"]
+            if last_version == __version__:
+                logger.log("You're currently using the lastest version !")
+            else:
+                for release in response:
+                    if release["name"] == __version__:
+                        logger.warn(
+                            f"You're not using the latest version '{__version__}' < '{last_version}'."
+                            f" Download the latest one here : https://github.com/{repo_name}/releases/")
+                        break
+                else:
+                    logger.warn("You're currently using a wip/unlisted version.")
+        except (requests.RequestException, AssertionError) as err:
+            logger.error("Couldn't verify if the bot is up to date.", err)
+
+        # Pterodactyl (for the hardware stats, optional)
+        pterodactyl = advanced["Pterodactyl"]
+        if pterodactyl["UsePterodactylAPI"]:
+            ptero_url = pterodactyl["URL"]
+            if ptero_url in ("", "https://"):
+                raise UncompletedOrMissing("Missing pterodactyl ptero_url.")
+            if ptero_url.endswith("/"):
+                raise Syntax("Your pterodactyl ptero_url mustn't end with '/'.")
+            ptero_token = pterodactyl["Token"]
+            if ptero_token in ("", "ptlc_"):
+                raise UncompletedOrMissing(
+                    f"Missing pterodactyl token. You can it here : {ptero_url}account/api/")
+            ptero_server_id = pterodactyl["ServerID"]
+            if ptero_server_id == "":
+                raise UncompletedOrMissing(
+                    "Missing pterodactyl server ID."
+                    " The ID is at the end of the server's link in the panel :"
+                    f" {ptero_url}server/" + ANSIEscape.underline + "1IdHere" + ANSIEscape.endc
+                )
+            cls = PterodactylShibbot
+            parameters = {
+                "ptero_url": ptero_url,
+                "ptero_token": ptero_token,
+                "ptero_server_id": ptero_server_id
+            }
+            kwargs.update(parameters)
+        logger.debug("All checks done and settings loaded.")
 
     # Starting the bot
     try:
